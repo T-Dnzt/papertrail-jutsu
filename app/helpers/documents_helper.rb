97df9b2db7f938b7ba2562f26e4bde566323ebe5
T-Dnzt <thi.denizet@gmail.com>
2014-09-14 22:07:02 +0700

    Add a list of versions to documents/edit.

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -1,2 +1,8 @@
 module DocumentsHelper
+
+  def find_version_author_name(version)
+    user = User.find_version_author(version) 
+    user ? user.name : ''
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   has_many :documents
+
+  def self.find_version_author(version)
+    find(version.terminator)   
+  end
+
 end
